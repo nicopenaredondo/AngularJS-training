b9280b3454e370fc22454a69ad9a4f255040f101
Nico Penaredondo <npenaredondo@gmail.com>
2014-09-14 00:25:26 +0800

    added save() and update() in app.js

--- larangular-test/public/app/app.js
+++ larangular-test/public/app/app.js
@@ -17,6 +17,31 @@ larangularTestApp.factory('Blog', function($http){
   return {
     get: function(){
       return $http.get('api/v1/blog');
+    },
+
+    save : function(blog){
+      return $http({
+        method : 'POST',
+        url : 'api/blog',
+        headers : { 'Content-Type' : 'application/x-www-form-urlencoded' },
+        data : { 'title' : blog.title, 'body' : blog.body }
+      });
+    },
+
+    update: function(blog){
+      return $http.({
+        method : 'PUT',
+        url : 'api/v1/blog/'+ id,
+        headers : { 'Content-Type' : 'application/x-www-form-urlencoded' },
+        data : { 'title' : blog.title, 'body' : blog.body }
+      });
+    },
+
+    delete: function(id){
+      return $http.({
+        method: 'DELETE',
+        url: 'api/v1/blog/' + id
+      });
     }
   }
 });
